9a902b44824cbff934eb80d5bc927675a21bb93d
Forrest Williams <ffwilliams2@alaska.edu>
2025-07-16 09:06:40 -0800

    add contains method

--- cxx/isce3/product/PolarGridParameters.cpp
+++ cxx/isce3/product/PolarGridParameters.cpp
@@ -3,11 +3,12 @@
 
 bool isce3::product::PolarGridParameters::
 contains(const double azdist, const double srange) const {
-    // const auto minAzimuth = (azimuthCenterPixel() - length()) * azimuthPixelSpacing();
-    // const auto halfAzimuthTimeInterval = azimuthPixelSpacing() / 2;
-    // const auto halfRangePixelSpacing = rangePixelSpacing() / 2;
-    // return aztime >= _sensingStart - halfAzimuthTimeInterval
-    //         and srange >= _startingRange - halfRangePixelSpacing
-    //         and aztime <= sensingStop() + halfAzimuthTimeInterval
-    //         and srange <= endingRange() + halfRangePixelSpacing;
+    const double endingRange = (width() * rangePixelSpacing()) + rangeStart();
+    const double endingAzimuth = (length() * azimuthPixelSpacing()) + azimuthStart();
+    const double halfAzimuthTimeInterval = azimuthPixelSpacing() / 2;
+    const double halfRangePixelSpacing = rangePixelSpacing() / 2;
+    return azdist >= azimuthStart() - halfAzimuthTimeInterval
+            and srange >= rangeStart() - halfRangePixelSpacing
+            and azdist <= endingAzimuth + halfAzimuthTimeInterval
+            and srange <= endingRange + halfRangePixelSpacing;
 }
